3d98604dfc89a53b88f0584abf7bbcfcd1bce6d7
Gola8011 <gola8011@gmail.com>
2025-06-07 11:10:28 +0530

    dist, and .parcel-cache from repo

--- dist/Netflix-GPT.6bd02f5a.js
+++
@@ -1,6 +0,0 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello World!!!";
-const root = document.getElementById("root");
-root.appendChild(heading);
-
-//# sourceMappingURL=Netflix-GPT.6bd02f5a.js.map
